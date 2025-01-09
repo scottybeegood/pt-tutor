bb8121e668a9fa367365e694dbbfdac413b64bf6
Scott Bastek <sbastek@pagerduty.com>
2025-01-08 22:05:01 -0500

    cleaning strings

--- pt_tutor/utils/functions.py
+++ pt_tutor/utils/functions.py
@@ -1,23 +1,33 @@
-import llm
+# import llm
+import re
+import unicodedata
 
-def update_conversation(n_submit, user_input, conversation_store):
-    if n_submit > 0 and user_input:
-        conversation_store.append({'role': 'user', 'text': user_input})
-        
-        conversation_history = "\n".join([f"{entry['role'].capitalize()}: {entry['text']}" for entry in conversation_store])
-        
-        response = llm.invoke(conversation_history)
-        conversation_store.append({'role': 'llm', 'text': response})
-        
-        return format_conversation(conversation_store), '', conversation_store  
-    return format_conversation(conversation_store), '', conversation_store  
+def clean_message(message):
+    lowercased = message.lower()
+    normalized = unicodedata.normalize('NFKC', lowercased)
+    cleaned = re.sub(r'[^a-zA-Z0-9\s\u00C0-\u017F]', '', normalized)
 
-# make this a seperate file eventually
-def format_conversation(conversation_store):
-    formatted_conversation = []
-    for entry in conversation_store:
-        if entry['role'] == 'user':
-            formatted_conversation.append(html.Div(f"{entry['text']}", className='user-text'))
-        else:
-            formatted_conversation.append(html.Div(f"{entry['text']}", className='llm-text'))
-    return formatted_conversation
+    return cleaned
+
+
+# def update_conversation(n_submit, user_input, conversation_store):
+#     if n_submit > 0 and user_input:
+#         conversation_store.append({'role': 'user', 'text': user_input})
+        
+#         conversation_history = "\n".join([f"{entry['role'].capitalize()}: {entry['text']}" for entry in conversation_store])
+        
+#         response = llm.invoke(conversation_history)
+#         conversation_store.append({'role': 'llm', 'text': response})
+        
+#         return format_conversation(conversation_store), '', conversation_store  
+#     return format_conversation(conversation_store), '', conversation_store  
+
+# # make this a seperate file eventually
+# def format_conversation(conversation_store):
+#     formatted_conversation = []
+#     for entry in conversation_store:
+#         if entry['role'] == 'user':
+#             formatted_conversation.append(html.Div(f"{entry['text']}", className='user-text'))
+#         else:
+#             formatted_conversation.append(html.Div(f"{entry['text']}", className='llm-text'))
+#     return formatted_conversation
--- pt_tutor/utils/instructions.py
+++ pt_tutor/utils/instructions.py
@@ -1,11 +1,11 @@
 chatbot_instructions = """
-You are a conversational chatbot. Use the familiar, informal mode. 
+You are a conversational chatbot. Engage with the user in a friendly manner. 
 
-The language of the conversation is: 
-{language}
-
-Respond to the user, keeping the conversation centered around the topic below:
-{topic}
+Specific instructions:
+1) The language of the conversation is: {language}
+2) Keep the conversation centered around the topic: {topic}
+3) Use the familiar, informal mode
+4) Keep responses to 1 or 2 sentences. 
 """
 
 corrector_instructions = """
