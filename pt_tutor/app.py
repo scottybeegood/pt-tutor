3b87eb6a9674615c80e9a5e3e133daa0e7fdeb6a
Scott Bastek <sbastek@pagerduty.com>
2025-03-31 23:33:39 +0100

    adding db logic without secrets

--- pt_tutor/app.py
+++ pt_tutor/app.py
@@ -1,14 +1,13 @@
 import streamlit as st 
-import uuid
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from utils.graph import graph 
+from utils.database import VocabDB
 from utils.functions import (
+    submit_username,
     get_topic_vocab,
-    load_progress,
     click_button,
     reset_button,
-    save_progress,
+    
 )
 
 st.set_page_config(layout="wide")
@@ -41,6 +40,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+if "username" not in st.session_state:
+    st.session_state.username = ""
+if 'username_submitted' not in st.session_state:
+    st.session_state.username_submitted = False
 if "student_messages" not in st.session_state:
     st.session_state.student_messages = []
 if "student_correction_messages" not in st.session_state:
@@ -56,97 +59,108 @@ if "last_correct_word" not in st.session_state:
 if "clicked" not in st.session_state:
     st.session_state.clicked = False
 
+db = VocabDB()
+
 sidebar = st.sidebar
 
-with sidebar:
-    topic = st.sidebar.radio(
-        "**Escolhe o tema que queres discutir e diz as palavras abaixo:**",
-        key="topic",
-        options=["Comer fora 🍽️", "Resumo do fim de semana 🍺", "Tempo ⛅"],
+if not st.session_state.username_submitted:
+    st.sidebar.text_input(
+        "**Crie ou insira o seu nome de perfil:**",
+        key="temp_username",
+        on_change=submit_username,
     )
-    topic_vocab = get_topic_vocab(topic)
-    if topic_vocab != st.session_state.topic_vocab:
-        st.session_state.correct_count = load_progress(topic)[0]
-        st.session_state.last_correct_word = load_progress(topic)[1]
+else:
+    st.sidebar.write(f"Olá, {st.session_state.username}!")
 
-    st.session_state.topic_vocab = topic_vocab
+    with sidebar:
+        topic = st.sidebar.radio(
+            "**Escolhe o tema que queres discutir e diz as palavras abaixo:**",
+            key="topic",
+            options=["Comer fora 🍽️", "Resumo do fim de semana 🍺", "Tempo ⛅"],
+        )
+        topic_vocab = get_topic_vocab(topic)
+        if topic_vocab != st.session_state.topic_vocab:
+            st.session_state.correct_count = db.load_progress(st.session_state.username, topic)[0]
+            st.session_state.last_correct_word = db.load_progress(st.session_state.username, topic)[1]
 
-    remaining_words = topic_vocab - set(st.session_state.correct_count)
-    remaining_word_dict = {word: 1 for word in remaining_words}
-    remaining_word_wordcloud = WordCloud(
-        width=800, 
-        height=425,
-        background_color='white',
-        min_font_size=20,
-        max_font_size=20,
-        random_state=42).generate_from_frequencies(remaining_word_dict)
-    st.sidebar.image(remaining_word_wordcloud.to_image(), use_container_width=True)
+        st.session_state.topic_vocab = topic_vocab
 
-    st.sidebar.write("**Palavras corretas**")
-
-    col1, col2 = st.sidebar.columns(2)
-    col1.markdown(f"<div class='box-style'>Última: <strong style='font-size:1.4em'>{st.session_state.last_correct_word}</strong></div>", unsafe_allow_html=True)
-    col2.markdown(f"<div class='box-style'>Totais: <strong style='font-size:1.4em'>{len(st.session_state.correct_count)}</strong></div>", unsafe_allow_html=True)
- 
-    if len(st.session_state.correct_count) > 0:
-        correct_word_wordcloud = WordCloud(
+        remaining_words = topic_vocab - set(st.session_state.correct_count)
+        remaining_word_dict = {word: 1 for word in remaining_words}
+        remaining_word_wordcloud = WordCloud(
             width=800, 
             height=425,
             background_color='white',
-            min_font_size=5,
-            max_font_size=100,
-            random_state=42).generate_from_frequencies(st.session_state.correct_count)
-        st.sidebar.image(correct_word_wordcloud.to_image(), use_container_width=True)
+            min_font_size=20,
+            max_font_size=20,
+            random_state=42).generate_from_frequencies(remaining_word_dict)
+        st.sidebar.image(remaining_word_wordcloud.to_image(), use_container_width=True)
 
-    st.sidebar.button("GUARDAR", key='launch', type="primary", on_click=click_button)
-    if st.session_state.clicked:
-        save_progress(topic, st.session_state.correct_count, st.session_state.last_correct_word)
-        st.sidebar.write("Guardado!")
-        reset_button()
+        st.sidebar.write("**Palavras corretas**")
 
-st.write("## Fala Português!")
+        col1, col2 = st.sidebar.columns(2)
+        col1.markdown(f"<div class='box-style'>Última: <strong style='font-size:1.4em'>{st.session_state.last_correct_word}</strong></div>", unsafe_allow_html=True)
+        col2.markdown(f"<div class='box-style'>Totais: <strong style='font-size:1.4em'>{len(st.session_state.correct_count)}</strong></div>", unsafe_allow_html=True)
+    
+        if len(st.session_state.correct_count) > 0:
+            correct_word_wordcloud = WordCloud(
+                width=800, 
+                height=425,
+                background_color='white',
+                min_font_size=5,
+                max_font_size=100,
+                random_state=42).generate_from_frequencies(st.session_state.correct_count)
+            st.sidebar.image(correct_word_wordcloud.to_image(), use_container_width=True)
 
-main_container = st.container()
+        st.sidebar.button("GUARDAR", key='launch', type="primary", on_click=click_button)
+        if st.session_state.clicked:
+            db.save_progress(st.session_state.username, topic, st.session_state.correct_count, st.session_state.last_correct_word)
+            st.sidebar.write("Guardado!")
+            reset_button()
 
-with main_container:
-    messages_container = st.container()
-    chat_area = messages_container.container(height=400)
+    st.write("## Fala Português!")
 
-    for i in range(len(st.session_state.student_messages)):
+    main_container = st.container()
+
+    with main_container:
+        messages_container = st.container()
+        chat_area = messages_container.container(height=400)
+
+        for i in range(len(st.session_state.student_messages)):
+            with chat_area.chat_message("student", avatar="😊"):
+                st.markdown(f"<div class='student-style'>{st.session_state.student_messages[i]}</div>", unsafe_allow_html=True)
+                st.markdown(f"<div class='student-correction-style'>{st.session_state.student_correction_messages[i]}</div>", unsafe_allow_html=True)
+            with chat_area.chat_message("tutor", avatar="🤖"):
+                st.markdown(f"<div class='tutor-style'>{st.session_state.tutor_messages[i]}</div>", unsafe_allow_html=True)
+
+    if prompt := st.chat_input("Fala aqui..."):
         with chat_area.chat_message("student", avatar="😊"):
-            st.markdown(f"<div class='student-style'>{st.session_state.student_messages[i]}</div>", unsafe_allow_html=True)
-            st.markdown(f"<div class='student-correction-style'>{st.session_state.student_correction_messages[i]}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div class='student-style'>{prompt}</div>", unsafe_allow_html=True)
+            st.session_state.student_messages.append(prompt)
+
+            response = graph.invoke(
+                {
+                    "messages": [prompt], 
+                    "core_convo": [prompt],
+                    "topic_vocab": topic_vocab,
+                    "correct_count": st.session_state.correct_count,
+                    "last_correct_word": st.session_state.last_correct_word,
+                    "topic": topic
+                },
+                config = {
+                    "configurable": {"thread_id": 42},
+                }
+            )
+            student_correction = response["corrections"][-1].content
+            st.session_state.student_correction_messages.append(student_correction)
+            st.markdown(f"""<div class='student-correction-style'>{student_correction}</div>""", unsafe_allow_html=True)
+
         with chat_area.chat_message("tutor", avatar="🤖"):
-            st.markdown(f"<div class='tutor-style'>{st.session_state.tutor_messages[i]}</div>", unsafe_allow_html=True)
+            tutor_response = response["core_convo"][-1].content
+            st.session_state.tutor_messages.append(tutor_response)
+            st.markdown(f"<div class='tutor-style'>{tutor_response}</div>", unsafe_allow_html=True)
 
-if prompt := st.chat_input("Fala aqui..."):
-    with chat_area.chat_message("student", avatar="😊"):
-        st.markdown(f"<div class='student-style'>{prompt}</div>", unsafe_allow_html=True)
-        st.session_state.student_messages.append(prompt)
-
-        response = graph.invoke(
-            {
-                "messages": [prompt], 
-                "core_convo": [prompt],
-                "topic_vocab": topic_vocab,
-                "correct_count": st.session_state.correct_count,
-                "last_correct_word": st.session_state.last_correct_word,
-                "topic": topic
-            },
-            config = {
-                "configurable": {"thread_id": 42},
-            }
-        )
-        student_correction = response["corrections"][-1].content
-        st.session_state.student_correction_messages.append(student_correction)
-        st.markdown(f"""<div class='student-correction-style'>{student_correction}</div>""", unsafe_allow_html=True)
-
-    with chat_area.chat_message("tutor", avatar="🤖"):
-        tutor_response = response["core_convo"][-1].content
-        st.session_state.tutor_messages.append(tutor_response)
-        st.markdown(f"<div class='tutor-style'>{tutor_response}</div>", unsafe_allow_html=True)
-
-        st.session_state.correct_count = response["correct_count"]
-        if response["last_correct_word"] != st.session_state.last_correct_word:
-            st.session_state.last_correct_word = response["last_correct_word"]
-            st.rerun()
+            st.session_state.correct_count = response["correct_count"]
+            if response["last_correct_word"] != st.session_state.last_correct_word:
+                st.session_state.last_correct_word = response["last_correct_word"]
+                st.rerun()
---
+++ pt_tutor/utils/database.py
@@ -0,0 +1,61 @@
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+class VocabDB:
+    def __init__(self):
+        self.db_path = 'pt_tutor/data/.streamlit/portuguese_app.db'
+        self.topic_filepath = {
+            'Comer fora 🍽️': 'pt_tutor/data/seed_vocab/dining_out.csv', 
+            'Resumo do fim de semana 🍺': 'pt_tutor/data/seed_vocab/weekend_recap.csv',
+            'Tempo ⛅': 'pt_tutor/data/seed_vocab/weather.csv',
+        }
+        self.init_database()
+
+    def init_database(self):
+        with sqlite3.connect(self.db_path) as conn:
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS progress (
+                    username TEXT,
+                    topic TEXT,
+                    portuguese_word TEXT,
+                    correct_count INTEGER,
+                    flag_last_correct_word INTEGER
+                )
+            ''')
+            conn.commit()
+
+
+    def load_progress(self, username: str, topic: str):
+        with sqlite3.connect(self.db_path) as conn:
+            df = pd.read_sql_query("""
+                select portuguese_word, correct_count, flag_last_correct_word
+                from progress
+                where username = ? and topic = ?
+            """, conn, params=(username, topic))
+
+            correct_count = df.set_index('portuguese_word')['correct_count'].to_dict()
+            correct_count = {k: v for k, v in correct_count.items() if v > 0}
+
+            try:
+                last_correct_word = df.loc[df['flag_last_correct_word'] == 1, 'portuguese_word'].iloc[0]
+            except IndexError:
+                last_correct_word = ''
+        
+            return (correct_count, last_correct_word)
+        
+
+    def save_progress(self, username: str, topic: str, correct_count: dict, last_correct_word: str):
+        words = list(correct_count.keys())
+        df = pd.DataFrame({
+            'username': username,
+            'topic': topic,
+            'portuguese_word': words,
+            'correct_count': [correct_count[word] for word in words],
+            'flag_last_correct_word': [1 if word == last_correct_word else 0 for word in words]
+        })
+
+        with sqlite3.connect(self.db_path) as conn:
+            conn.execute('delete from progress where username = ? and topic = ?', (username, topic))
+
+            df.to_sql('progress', conn, if_exists='append', index=False)
