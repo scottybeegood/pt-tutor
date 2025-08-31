7ddf3cf7811471c92612e1b9af6d92256db89803
Scott Bastek <sbastek@pagerduty.com>
2025-08-31 23:15:51 +0100

    fixing translation appearance timing

--- pt_tutor/text_chat.py
+++ pt_tutor/text_chat.py
@@ -104,11 +104,11 @@ def run_text_chat():
             tutor_response = response["core_convo"][-1].content
             st.session_state.tutor_messages.append(tutor_response)
             st.markdown(f"<div class='tutor-style'>{tutor_response}</div>", unsafe_allow_html=True)
-            st.button(label="Traduzir última", key='translate', type="secondary", on_click=click_translate_button)
-            if st.session_state.clicked_translate:
+            if st.button(label="Traduzir última", key='translate', type="secondary") # , on_click=click_translate_button)
+            # if st.session_state.clicked_translate:
                 translate_last()
                 st.markdown(f"""<div class='tutor-translate-style'>{st.session_state.last_tutor_message_translated}</div>""", unsafe_allow_html=True)
-                reset_translate_button()
+                # reset_translate_button()
 
             st.session_state.correct_count = response["correct_count"]
             if response["last_correct_word"] != st.session_state.last_correct_word:
