36d5d6af2b5235c4b905c622f9a379970a30e800
Scott Bastek <sbastek@pagerduty.com>
2025-08-31 23:06:29 +0100

    moving translate appear logic out of sidebar, below chat in main

--- pt_tutor/text_chat.py
+++ pt_tutor/text_chat.py
@@ -57,11 +57,11 @@ def run_text_chat():
             st.sidebar.image(correct_word_wordcloud.to_image(), use_container_width=True)
 
         st.sidebar.button(label="Traduzir última", key='translate', type="secondary", on_click=click_translate_button)
-        if st.session_state.clicked_translate:
-            translate_last()
-            st.sidebar.write(st.session_state.last_tutor_message_translated)
-            st.markdown(f"""<div class='tutor-translate-style'>{st.session_state.last_tutor_message_translated}</div>""", unsafe_allow_html=True)
-            reset_translate_button()
+        # if st.session_state.clicked_translate:
+        #     translate_last()
+        #     st.sidebar.write(st.session_state.last_tutor_message_translated)
+        #     st.markdown(f"""<div class='tutor-translate-style'>{st.session_state.last_tutor_message_translated}</div>""", unsafe_allow_html=True)
+        #     reset_translate_button()
 
         st.sidebar.button(label="GUARDAR", key='launch', type="primary", on_click=click_button)
         if st.session_state.clicked:
@@ -111,9 +111,13 @@ def run_text_chat():
             tutor_response = response["core_convo"][-1].content
             st.session_state.tutor_messages.append(tutor_response)
             st.markdown(f"<div class='tutor-style'>{tutor_response}</div>", unsafe_allow_html=True)
-            # st.markdown(f"""<div class='tutor-translate-style'>{st.session_state.last_tutor_message_translated}</div>""", unsafe_allow_html=True)
 
             st.session_state.correct_count = response["correct_count"]
             if response["last_correct_word"] != st.session_state.last_correct_word:
                 st.session_state.last_correct_word = response["last_correct_word"]
                 st.rerun()
+
+        if st.session_state.clicked_translate:
+            translate_last()
+            st.markdown(f"""<div class='tutor-translate-style'>{st.session_state.last_tutor_message_translated}</div>""", unsafe_allow_html=True)
+            reset_translate_button()
