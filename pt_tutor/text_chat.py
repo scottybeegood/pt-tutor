13b85a23ee84de244055a6c4ac0310c6189c1eb7
Scott Bastek <sbastek@pagerduty.com>
2025-10-13 18:42:54 +0100

    moving pieces around

--- pt_tutor/text_chat.py
+++ pt_tutor/text_chat.py
@@ -64,8 +64,6 @@ def run_text_chat():
             st.sidebar.write("Guardado!")
             reset_button()
 
-        st.write(f'last_tutor_message_translated: {st.session_state.last_tutor_message_translated}')
-
     st.write("## Fala Português!")
 
     # starting main section 
@@ -82,7 +80,7 @@ def run_text_chat():
             with chat_area.chat_message(name="tutor", avatar="🤖"):
                 st.markdown(f"<div class='tutor-style'>{st.session_state.tutor_messages[i]}</div>", unsafe_allow_html=True)
                 if i == len(st.session_state.student_messages) - 1:
-                    st.button(label="Traduzir última", key='translate2', type="secondary", on_click=translate_last)
+                    st.button(label="Traduzir última", key='translate', type="secondary", on_click=translate_last)
                     st.markdown(f"""<div class='tutor-translate-style'>{st.session_state.last_tutor_message_translated}</div>""", unsafe_allow_html=True)
 
     if prompt := st.chat_input("Fala aqui..."):
@@ -111,17 +109,20 @@ def run_text_chat():
             tutor_response = response["core_convo"][-1].content
             st.session_state.tutor_messages.append(tutor_response)
             st.markdown(f"<div class='tutor-style'>{tutor_response}</div>", unsafe_allow_html=True)
-            st.button(label="Traduzir última", key='translate', type="secondary", on_click=translate_last)
+            #st.button(label="Traduzir última", key='translate', type="secondary", on_click=translate_last)
 
         st.session_state.correct_count = response["correct_count"]
         if response["last_correct_word"] != st.session_state.last_correct_word:
             st.session_state.last_correct_word = response["last_correct_word"]
-            st.session_state.need_rerun = True
 
-    if st.session_state.need_rerun:
-        trigger_rerun()
+    st.rerun()
 
-    if st.session_state.clicked_translate:
-        reset_translate_button()
+    # st.session_state.need_rerun = True
+
+    # if st.session_state.need_rerun:
+    #     trigger_rerun()
+
+    # if st.session_state.clicked_translate:
+    #     reset_translate_button()
 
 
