dfb7ebb55e35263cfa3dff085db85fec8c1b66a0
Scott Bastek <sbastek@pagerduty.com>
2025-10-21 13:30:54 +0100

    typo

--- pt_tutor/chat.py
+++ pt_tutor/chat.py
@@ -121,7 +121,7 @@ def run_chat():
             record_audio(st.session_state.recording, question_file)
             user_input = transcribe_audio(question_file)
 
-    if user_input:    
+    if st.session_state.user_input:    
         with chat_area.chat_message(name="student", avatar="😊"):
             st.markdown(f"<div class='student-style'>{user_input}</div>", unsafe_allow_html=True)
             st.session_state.student_messages.append(user_input)
