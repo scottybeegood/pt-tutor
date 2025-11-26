8a9f468009cfa38b12cfffdb578b87d9255b78e0
Scott Bastek <sbastek@pagerduty.com>
2025-11-26 09:39:52 +0000

    trying 3.5

--- pt_tutor/utils/functions.py
+++ pt_tutor/utils/functions.py
@@ -21,7 +21,7 @@ openai_org_id = os.getenv("OPENAI_ORG_ID") or st.secrets.get("OPENAI_ORG_ID")
 llm = ChatOpenAI(
     api_key=openai_api_key,
     organization=openai_org_id,
-    model="gpt-5-mini",
+    model="gpt-3.5-turbo",
     temperature=1.0
 )
 
